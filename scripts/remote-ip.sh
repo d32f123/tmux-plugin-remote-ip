39be6304fc3213d6b08b97077be8b6c75b701858
Andrey Nesterov <anesterov@anesterov.xyz>
2020-08-27 01:53:11 +0300

    Refresh IP only every 60 calls

--- scripts/remote-ip.sh
+++ scripts/remote-ip.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
-host -t A anesterov.xyz | sed -E 's/.*has address (.*)$/\1/g'
+local i="$(tmux show-option -gvq @remote-ip-iter)"
+local curr_ip="$(tmux show-option -gvq @remote-ip)"
+(( i = ${i:=0} + 1 ))
+tmux set-option -gq @remote-ip-iter "$i"
+(( i % 60 )) && [ -n "$curr_ip" ] && echo "$curr_ip" && exit
+
+
+local ip="$(host -t A anesterov.xyz | sed -nE 's/.*has address (.*)$/\1/gp')"
+ip=${ip:-No IP}
+tmux set-option -gq @remote-ip "$ip"
+echo "$ip"
