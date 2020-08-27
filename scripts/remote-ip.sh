98dac0bf5961c6195b58e6f428a44d4cf8937b98
Andrey Nesterov <anesterov@anesterov.xyz>
2020-08-27 09:46:54 +0300

    Use ifconfig.me to fetch IP

--- scripts/remote-ip.sh
+++ scripts/remote-ip.sh
@@ -7,7 +7,6 @@ tmux set-option -gq @remote-ip-iter "$i"
 (( i % 60 )) && [ -n "$curr_ip" ] && echo "$curr_ip" && exit
 
 
-local ip="$(host -t A anesterov.xyz | sed -nE 's/.*has address (.*)$/\1/gp')"
-ip=${ip:-No IP}
+local ip="$(curl ifconfig.me 2>/dev/null || echo "No IP")"
 tmux set-option -gq @remote-ip "$ip"
 echo "$ip"
